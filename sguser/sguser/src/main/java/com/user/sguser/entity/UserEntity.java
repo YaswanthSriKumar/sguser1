019996d0ffdb16ebd2dd29803b4e9b8bba48dcde
kranthiyalavarthi <kranthiyalavarthi89462@gmail.com>
2025-05-28 19:20:18 +0530

    first1

--- sguser/sguser/src/main/java/com/user/sguser/entity/UserEntity.java
+++ sguser/sguser/src/main/java/com/user/sguser/entity/UserEntity.java
@@ -13,5 +13,59 @@ public class UserEntity {
     @GeneratedValue
     private UUID customerId;
     private String selectedType;
+    private int selectedTypeId;
+    private String customerName;
+    private String customerContact;
 
+
+    public UUID getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(UUID customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getSelectedType() {
+        return selectedType;
+    }
+
+    public void setSelectedType(String selectedType) {
+        this.selectedType = selectedType;
+    }
+
+    public int getSelectedTypeId() {
+        return selectedTypeId;
+    }
+
+    public void setSelectedTypeId(int selectedTypeId) {
+        this.selectedTypeId = selectedTypeId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerContact() {
+        return customerContact;
+    }
+
+    public void setCustomerContact(String customerContact) {
+        this.customerContact = customerContact;
+    }
+
+    @Override
+    public String toString() {
+        return "UserEntity{" +
+                "customerId=" + customerId +
+                ", selectedType='" + selectedType + '\'' +
+                ", selectedTypeId=" + selectedTypeId +
+                ", customerName='" + customerName + '\'' +
+                ", customerContact='" + customerContact + '\'' +
+                '}';
+    }
 }
