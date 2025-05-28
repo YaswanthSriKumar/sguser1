6faa6a0f4559f49c83ba1554ae2b198f2bbd5fd4
kranthiyalavarthi <kranthiyalavarthi89462@gmail.com>
2025-05-28 23:37:41 +0530

    first3

--- sguser/sguser/src/main/java/com/user/sguser/controller/UserController.java
+++ sguser/sguser/src/main/java/com/user/sguser/controller/UserController.java
@@ -15,6 +15,7 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    // saving the new User detils
     @PostMapping("/saveuser")
     public ResponseEntity<String> saveUserDetails(@RequestBody UserEntity userEntity)
     {
